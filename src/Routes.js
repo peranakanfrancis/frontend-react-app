02b168dc9f72926bbae5155a856886d5dec801a7
Jay <jvelayud@gmail.com>
2017-08-18 18:54:22 -0400

    Upgrading

--- src/Routes.js
+++ src/Routes.js
@@ -14,8 +14,8 @@ export default ({ childProps }) =>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
-    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+    <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
